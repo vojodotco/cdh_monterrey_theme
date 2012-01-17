762427e93a331f3b96b71a4561274873ec3318a1
Rahul Bhargava <rahulb@media.mit.edu>
2012-01-17 14:21:19 -0500

    vojo_cdh_monterrey_feature.features.incvojo_cdh_monterrey_feature.features.menu_custom.incvojo_cdh_monterrey_feature.infovojo_cdh_monterrey_feature.modulevojo_cdh_monterrey_feature.openlayers_presets.incvojo_cdh_monterrey_feature.pages_default.incvojo_cdh_monterrey_feature.panels_default.incvojo_cdh_monterrey_feature.views_default.inc

---
+++ templates/views-view-unformatted--cdhm-og-search--cdhm-search-page.tpl.php
@@ -0,0 +1,11 @@
+<?php
+// $Id: views-view-unformatted.tpl.php,v 1.6 2008/10/01 20:52:11 merlinofchaos Exp $
+/**
+ * @file views-view-unformatted.tpl.php
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>
+
+<?php include("story-list-page.inc"); ?>
